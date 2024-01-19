18665269ccf0525e2f9291666e43fac34c764d50
Sanika0923 <157109400+Sanika0923@users.noreply.github.com>
2024-01-19 15:08:06 +0530

    Add files via upload

---
+++ 3Tgame.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+main(){
+    cout<<"\n\nTick Cross Game"<<endl;
+    cout<<"\tPlayer1[X] \n\tPlayer2[O]\n\n";
+
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  1  |  2  |   3 \n";
+    cout<<"\t\t_____|_____|______\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  4  |  5  |   6 \n";
+    cout<<"\t\t_____|_____|______\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  7  |  8  |   9 \n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\n\t Player1 [X] turn:";
+}
